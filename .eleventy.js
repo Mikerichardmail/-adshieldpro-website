c212b1fa1327b00159e342c27e6b4f20cedcdb20
Mikerichardmail <shubhamjainatoz@gmail.com>
2026-04-06 16:39:24 +0530

    Update site templates and eleventy configuration

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,17 @@
 module.exports = function (eleventyConfig) {
-  eleventyConfig.addPassthroughCopy({ "src/assets": "assets" });
+  // Pass static assets directly to _site root
+  eleventyConfig.addPassthroughCopy({ "site/styles.css": "styles.css" });
+  eleventyConfig.addPassthroughCopy({ "site/icon.svg": "icon.svg" });
+  eleventyConfig.addPassthroughCopy({ "site/ui-image.png": "ui-image.png" });
+  eleventyConfig.addPassthroughCopy({ "site/robots.txt": "robots.txt" });
+  eleventyConfig.addPassthroughCopy({ "site/sitemap.xml": "sitemap.xml" });
 
   return {
     dir: {
       input: "src",
       output: "_site",
-      includes: "_includes"
+      includes: "_includes",
+      data: "_data"
     }
   };
 };
